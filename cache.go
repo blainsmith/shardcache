e561c52a634f5fa229a33f8c961ceba9ab40a2ea
Blain Smith <rebelgeek@blainsmith.com>
2021-02-07 14:25:51 -0500

    write some godoc

--- cache.go
+++ cache.go
@@ -9,12 +9,14 @@ type shard struct {
 	items map[uint64]interface{}
 }
 
+// ShardCache will keep track and shard keys based on the number of shards it holds around mutexes so this is safe for concurrent use
 type ShardCache struct {
 	mu     sync.RWMutex
 	n      uint64
 	shards []shard
 }
 
+// New returns a ShardCache with the number of shards which must be a power of 2
 func New(shards uint64) *ShardCache {
 	if shards%2 > 0 {
 		panic("shards must be a power of 2")
@@ -26,6 +28,7 @@ func New(shards uint64) *ShardCache {
 	}
 }
 
+// Get a key from the cache and only use a read lock to access it
 func (sl *ShardCache) Get(key uint64) interface{} {
 	sl.mu.RLock()
 	defer sl.mu.RUnlock()
@@ -33,6 +36,7 @@ func (sl *ShardCache) Get(key uint64) interface{} {
 	return sl.shards[key&sl.n].items[key]
 }
 
+// Set a key in the cache and use a write lock only for the shard that will hold the value leaving all other shards available for reads
 func (sl *ShardCache) Set(key uint64, value interface{}) {
 	sl.mu.RLock()
 	defer sl.mu.RUnlock()
@@ -47,6 +51,7 @@ func (sl *ShardCache) Set(key uint64, value interface{}) {
 	sl.shards[key&sl.n].items[key] = value
 }
 
+// Delete a key in the cache and use a write lock only for the shard that has the value leaving all other shards available for reads
 func (sl *ShardCache) Delete(key uint64) {
 	sl.mu.RLock()
 	defer sl.mu.RUnlock()
